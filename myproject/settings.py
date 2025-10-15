775a2a5d33c9e12625f806182d89d8b6c0a3bc0e
Sarojini <sarojinidevan2804@gmail.com>
2025-10-15 12:19:07 +0530

    Added Netlify frontend to CORS allowed origins

--- myproject/settings.py
+++ myproject/settings.py
@@ -129,7 +129,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://68df830e708773b4e8cdfc61--kaleidoscopic-flan-52ce87.netlify.app",
+]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
